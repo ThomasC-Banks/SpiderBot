665147b40b9a5fce1b0d2ec029eb7cdd38a7a9c9
CThomas <corde.thomas1@gmail.com>
2026-05-13 14:42:17 +0200

    feat: face lcd

---
+++ SpiderBot/main/face.h
@@ -0,0 +1,14 @@
+#ifndef FACE_H
+#define FACE_H
+
+void face_init(void);
+void face_normal(void);
+void face_happy(void);
+void face_blink(void);
+void face_look_left(void);
+void face_look_right(void);
+void face_sleepy(void);
+void face_excited(void);
+void face_wink(void);
+
+#endif
